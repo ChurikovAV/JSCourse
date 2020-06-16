ec2aadfecf7d61b0c301143e395f7bb4bdfd0efd
Anatoliy <anatoliy.churikov@gmail.com>
2020-06-16 15:17:20 +0700

    My first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+console.log('arr' + " - object");
+console.log(4 + +'5');
+
+let incr = 10;
+let decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
